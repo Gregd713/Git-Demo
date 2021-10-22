96ade5b1ba35f4aab5559ff7ba4be347c690a62d
Gregory Delgado <gregd92694@yahoo.com>
2021-10-22 16:02:49 -0500

    Finished completing randomizer functions for itinerary.

--- scriptproject.js
+++ scriptproject.js
@@ -2,17 +2,17 @@
 
 
 
-let places = ['Houston','El Paso','Austin','San Antonio'];
+let places = [' Houston',' El Paso',' Austin',' San Antonio'];
 let Houston = places[0];
 
-let restaurants = ['Mcdonalds','Taco Bell','Whataburger','Burger King'];
+let restaurants = [' Mcdonalds',' Taco Bell',' Whataburger',' Burger King'];
 let Mcdonalds = restaurants[0];
 
-let activities = ['Shopping','Theater','Movies','Picnic'];
+let activities = [' Shopping',' Theater',' Movies',' Picnic'];
 let  Shopping = activities[0];
 
-let transportation = ['Walking','Biking','Uber','Rental Car'];
-let Walking = activities[0];
+let transportation = [' Walking',' Biking',' Uber',' Rental Car'];
+let Walking = transportation[0];
 
 var randomG = Math.floor(Math.random() * 4);
 console.log(randomG)
@@ -30,21 +30,33 @@ function placesToGo(toGoOut){
     if(randomG === 0){toGoOut = places[0];}
     else if(randomG === 1){toGoOut = places[1];}
     else if(randomG === 2){toGoOut = places[2];}
-    else if(randomG === 3){toGoOut = places[3];}
+    else if(randomG === 3) {toGoOut = places[3];}
     return toGoOut;}
 
 function placesToEat(toEat){
     if(randomE ===0){toEat = restaurants[0];}
-
+    else if(randomE ===1){toEat = restaurants[1];}
+    else if(randomE ===2){toEat = restaurants[2];}
+    else if(randomE ===3){toEat = restaurants[3];}
+    return toEat;
 }
 
 function thingsToDo(toDo){
+    if(randomD ===0){toDo = activities[0];}
+    else if(randomD ===1){toDo = activities[1];}
+    else if(randomD ===2){toDo = activities[2];}
+    else if(randomD ===3){toDo = activities[3];}
+    return toDo;
 
 }
 
 function howIWillGetThere(getThere){
+    if(randomT ===0){getThere = transportation[0];}
+    else if(randomT ===1){getThere = transportation[1];}
+    else if(randomT ===2){getThere = transportation[2];}
+    else if(randomT ===3){getThere = transportation[3];}
+    return getThere;
 
 }
 
-alert("Travel itinerary: \nDestination: \nDining: \nActivities: \nTransportation:");
-alert(placesToGo());
+alert("Travel itinerary:" +"\n"+"\nDestination:"+placesToGo()+ "\nDining:"+placesToEat() + "\nActivities:"+thingsToDo()+ "\nTransportation:"+howIWillGetThere());
