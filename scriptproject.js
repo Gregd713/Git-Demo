c6f5175e59160afa8c732c1271e7f38b797039e4
Gregory Delgado <gregd92694@yahoo.com>
2021-10-22 20:19:15 -0500

    Completed worksheet to allow for 'yes' changes to be made to itinerary. Still have to do the portion for no changes to be made.

--- scriptproject.js
+++ scriptproject.js
@@ -1,7 +1,6 @@
 "Use Strict";
 
 
-
 let places = [' Houston',' El Paso',' Austin',' San Antonio'];
 let Houston = places[0];
 
@@ -14,19 +13,10 @@ let  Shopping = activities[0];
 let transportation = [' Walking',' Biking',' Uber',' Rental Car'];
 let Walking = transportation[0];
 
-var randomG = Math.floor(Math.random() * 4);
-console.log(randomG)
-
-var randomE = Math.floor(Math.random()*4);
-console.log(randomE) 
-
-var randomD = Math.floor(Math.random()*4);
-console.log(randomD)
-
-var randomT = Math.floor(Math.random()*4);
-console.log(randomT)
 
 function placesToGo(toGoOut){
+    var randomG = Math.floor(Math.random() * 4);
+    console.log(randomG)
     if(randomG === 0){toGoOut = places[0];}
     else if(randomG === 1){toGoOut = places[1];}
     else if(randomG === 2){toGoOut = places[2];}
@@ -34,6 +24,8 @@ function placesToGo(toGoOut){
     return toGoOut;}
 
 function placesToEat(toEat){
+    var randomE = Math.floor(Math.random()*4);
+    console.log(randomE)
     if(randomE ===0){toEat = restaurants[0];}
     else if(randomE ===1){toEat = restaurants[1];}
     else if(randomE ===2){toEat = restaurants[2];}
@@ -42,6 +34,8 @@ function placesToEat(toEat){
 }
 
 function thingsToDo(toDo){
+    var randomD = Math.floor(Math.random()*4);
+    console.log(randomD)
     if(randomD ===0){toDo = activities[0];}
     else if(randomD ===1){toDo = activities[1];}
     else if(randomD ===2){toDo = activities[2];}
@@ -51,6 +45,8 @@ function thingsToDo(toDo){
 }
 
 function howIWillGetThere(getThere){
+    var randomT = Math.floor(Math.random()*4);
+    console.log(randomT)
     if(randomT ===0){getThere = transportation[0];}
     else if(randomT ===1){getThere = transportation[1];}
     else if(randomT ===2){getThere = transportation[2];}
@@ -58,5 +54,25 @@ function howIWillGetThere(getThere){
     return getThere;
 
 }
-
 alert("Travel itinerary:" +"\n"+"\nDestination:"+placesToGo()+ "\nDining:"+placesToEat() + "\nActivities:"+thingsToDo()+ "\nTransportation:"+howIWillGetThere());
+
+let userChange = prompt("Would you like to make any changes?")
+if(userChange === "yes"||"Yes"){ 
+    let changePlans =prompt("What changes would you like to make?")
+    if(changePlans === "1" ||"Destination"||"destination"){ 
+         placesToGo();
+        alert("Travel itinerary:" +"\n"+"Destination"+placesToGo()+ "\nDining:"+placesToEat.call() + "\nActivities:"+thingsToDo.call()+ "\nTransportation:"+howIWillGetThere.call());
+    }
+    else if(changePlans === "2"||"Dining"||"dining"){
+       placesToEat();
+            alert("Travel itinerary:" +"\n"+"Destination"+placesToGo.call()+ "\nDining:"+placesToEat() + "\nActivities:"+thingsToDo.call()+ "\nTransportation:"+howIWillGetThere.call());
+        }
+    else if(changePlans ==="3"||"Activities"||"activities"){
+        thingsToDo();
+        alert("Travel itinerary:" +"\n"+"Destination"+placesToGo.call()+ "\nDining:"+placesToEat.call() + "\nActivities:"+thingsToDo()+ "\nTransportation:"+howIWillGetThere.call())
+        }
+    else if(changePlans==="4"||"Transportation"||"transportation"){
+        howIWillGetThere();
+        alert("Travel itinerary:" +"\n"+"Destination"+placesToGo.call()+ "\nDining:"+placesToEat.call() + "\nActivities:"+thingsToDo.call()+ "\nTransportation:"+howIWillGetThere())
+    }
+}
